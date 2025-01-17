c9bd23691f736ac2a13916a926c47d42093dc23b
PramodRavindu00 <pramod2000.ravindu@gmail.com>
2025-01-17 20:48:25 +0530

    user distribution chart completed. data fetching errors fixed

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,26 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter", "sans-serif"],
-      },
-      backgroundImage: {
-        "bg-content":
-          "linear-gradient(to bottom, #0e3c2c  0%, #456e68  50%, #092922 100%)",
-      },
-      animation: {
-        fade: "fadeIn 0.5s ease-in-out",
-      },
-      keyframes: {
-        fadeIn: {
-          "0%": { opacity: "0" },
-          "100%": { opacity: "1" },
-        },
-      },
-    },
+  	extend: {
+  		fontFamily: {
+  			sans: [
+  				'Inter',
+  				'sans-serif'
+  			]
+  		},
+  		backgroundImage: {
+  			'bg-content': 'linear-gradient(to bottom, #0e3c2c  0%, #456e68  50%, #092922 100%)'
+  		},
+  		animation: {
+  			fade: 'fadeIn 0.5s ease-in-out'
+  		},
+  		keyframes: {
+  			fadeIn: {
+  				'0%': {
+  					opacity: '0'
+  				},
+  				'100%': {
+  					opacity: '1'
+  				}
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
