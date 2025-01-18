3def53d84216d6e044369514eb0676047ef4e2a9
PramodRavindu00 <pramod2000.ravindu@gmail.com>
2025-01-19 02:54:36 +0530

    OTP send to reset password backend endpoint completed

--- server/routes/userRoutes.mjs
+++ server/routes/userRoutes.mjs
@@ -4,6 +4,7 @@ import {
   getPreferredVeggieCount,
   getSingleUser,
   getUserDistribution,
+  sendOTP,
   updateUserPreferences,
 } from "../controllers/UserControllers.mjs";
 import { authenticate, authorize } from "../middlewares/authMiddleWares.mjs";
@@ -16,4 +17,5 @@ router.get(`${prefix}/getUserDetails/:id`, getSingleUser);
 router.patch(`${prefix}/updatePreferences/:id`, updateUserPreferences);
 router.get(`${prefix}/getUserDistribution`, getUserDistribution);
 router.get(`${prefix}/getVeggieCount`, getPreferredVeggieCount);
+router.post(`${prefix}/sendOTP`, sendOTP);
 export default router;
